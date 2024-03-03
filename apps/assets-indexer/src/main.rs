1174bcabe17f47b264d3ebfe21472d8ffe3ddc60
Joaquin Gonzalez <jpgonzalezra@gmail.com>
2024-03-03 14:33:57 -0300

    added redis stream for productor and consumers

--- apps/assets-indexer/src/main.rs
+++ apps/assets-indexer/src/main.rs
@@ -1,3 +1,75 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env, time::Duration};
+
+use common::redis::redis_client_factory;
+use redis::{
+    streams::{StreamReadOptions, StreamReadReply},
+    AsyncCommands, RedisResult,
+};
+use tokio::time::sleep;
+
+async fn ensure_stream_and_group_exist(
+    conn: &mut redis::aio::Connection,
+    stream_key: &str,
+    group_name: &str,
+) -> RedisResult<()> {
+    // Try to create the group. If the group already exists, this command will fail, so ignore the specific error of an existing group.
+    let create_group_result: RedisResult<()> = conn
+        .xgroup_create_mkstream(stream_key, group_name, "$")
+        .await;
+    match create_group_result {
+        Ok(_) => println!(
+            "Group '{}' created for the stream '{}'.",
+            group_name, stream_key
+        ),
+        Err(ref e)
+            if e.kind() == redis::ErrorKind::ExtensionError
+                && e.to_string().contains("BUSYGROUP") =>
+        {
+            println!(
+                "The group '{}' already exists for the stream '{}'.",
+                group_name, stream_key
+            );
+        }
+        Err(e) => return Err(e), // Propagate other errors.
+    }
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut redis_conn = redis_client_factory()
+        .get_async_connection()
+        .await
+        .expect("Error on acquiring redis connection");
+
+    ensure_stream_and_group_exist(
+        &mut redis_conn,
+        "ASSETS_INDEXER_STREAM",
+        "ASSETS_INDEXER_GROUP",
+    )
+    .await?;
+
+    let consumer_name: String = env::var("CONSUMER_NAME").unwrap();
+    let opts = StreamReadOptions::default().group("ASSETS_INDEXER_GROUP", consumer_name);
+
+    let stream_key = "ASSETS_INDEXER_STREAM";
+
+    loop {
+        let results: RedisResult<StreamReadReply> =
+            redis_conn.xread_options(&[stream_key], &[">"], &opts).await;
+
+        match results {
+            Ok(reply) => {
+                for stream in reply.keys {
+                    for message in stream.ids {
+                        println!("ID: {}, Values: {:?}", message.id, message.map);
+                    }
+                }
+            }
+            Err(e) => println!("Error reading from stream: {}", e),
+        }
+
+        sleep(Duration::from_millis(500)).await;
+    }
 }
--- apps/chain-watcher/src/main.rs
+++ apps/chain-watcher/src/main.rs
@@ -1,20 +1,70 @@
+use common::redis::redis_client_factory;
 use dotenv::dotenv;
 use ethers::providers::{Http, Middleware, Provider};
-use ethers::types::{transaction, Transaction};
+use ethers::types::Transaction;
+use ethers::utils::hex;
+use redis::AsyncCommands;
+use serde::{Deserialize, Serialize};
 use std::env;
 use std::sync::Arc;
 use std::time::Duration;
 use tokio::time::sleep;
 
+#[derive(Serialize, Deserialize)]
+struct SummaryTransaction {
+    pub hash: String,
+    pub block_hash: String,
+    pub block_number: u64,
+    pub chain_id: String,
+    pub input: String,
+    pub from: String,
+    pub to: String,
+    pub nonce: String,
+    pub transaction_index: u64,
+    pub value: String,
+}
+
+impl From<Transaction> for SummaryTransaction {
+    fn from(tx: Transaction) -> Self {
+        SummaryTransaction {
+            hash: tx.hash.to_string(),
+            block_hash: tx
+                .block_hash
+                .map_or_else(|| "None".to_string(), |h| h.to_string()),
+            block_number: tx
+                .block_number
+                .map_or_else(|| 0, |block_number| block_number.as_u64()),
+            chain_id: tx
+                .chain_id
+                .map_or_else(|| 1.to_string(), |chain_id| chain_id.to_string()),
+            input: hex::encode(tx.input),
+            from: tx.from.to_string(),
+            to: tx
+                .to
+                .map_or_else(|| "None".to_string(), |to| to.to_string()),
+            nonce: tx.nonce.to_string(),
+            transaction_index: tx
+                .transaction_index
+                .map_or_else(|| 0, |index| index.as_u64()),
+            value: tx.value.to_string(),
+        }
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
+
+    let mut redis_conn = redis_client_factory()
+        .get_async_connection()
+        .await
+        .expect("Error on acquiring redis connection");
 
     let rpc_url = env::var("ETHEREUM_RPC_URL").unwrap();
     let http_provider = Provider::<Http>::try_from(rpc_url)?;
 
     let provider = Arc::new(http_provider);
-    let start_block = 0;
+    let start_block = 19249009;
     let end_block = provider.get_block_number().await?;
 
     println!("Indexing from block {} to block {}", start_block, end_block);
@@ -30,10 +80,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             );
 
             for transaction in block.transactions {
-                println!("Transaction: {:?}", transaction);
+                let summary_transaction: SummaryTransaction = transaction.into();
+                let message = serde_json::to_string(&summary_transaction).unwrap();
+                let _: () = redis_conn
+                    .xadd("ASSETS_INDEXER_STREAM", "*", &[("message", &message)])
+                    .await
+                    .unwrap();
             }
         }
-        sleep(Duration::from_secs(5)).await;
+        sleep(Duration::from_secs(1)).await;
     }
 
     Ok(())
