ed6f371ca2c934d8ef5dda35f2d6406c6518b344
Joaquin Gonzalez <jpgonzalezra@gmail.com>
2024-03-05 18:05:49 -0300

    keeping indexes (start and end block) up to date

--- apps/chain-watcher/src/main.rs
+++ apps/chain-watcher/src/main.rs
@@ -18,59 +18,68 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let http_provider = Provider::<Http>::try_from(rpc_url)?;
     let provider = Arc::new(http_provider);
 
-    let start_block = 19249009;
-    let end_block = provider.get_block_number().await?;
-    println!("Indexing from block {} to block {}", start_block, end_block);
-
     let cpu_cores = num_cpus::get();
     println!("Using {} workers.", cpu_cores);
 
-    for block_number in start_block..=end_block.as_u64() {
-        let provider = provider.clone();
+    loop {
+        let mut start_block = 19249009;
+        let end_block = provider.get_block_number().await?.as_u64();
+        println!("Indexing from block {} to block {}", start_block, end_block);
 
-        if let Ok(Some(block)) = provider.get_block_with_txs(block_number).await {
-            println!(
-                "Block Number {}: {} transactions",
-                block.number.unwrap(),
-                block.transactions.len()
-            );
+        for block_number in start_block..=end_block {
+            let provider = provider.clone();
 
-            let mut futures = FuturesUnordered::new();
-            for transaction in block.transactions {
-                let provider = provider.clone();
-                let pool_clone = pool.clone();
+            if let Ok(Some(block)) = provider.get_block_with_txs(block_number).await {
+                println!(
+                    "Block Number {}: {} transactions",
+                    block.number.unwrap(),
+                    block.transactions.len()
+                );
 
-                futures.push(task::spawn(async move {
-                    if let Ok(Some(receipt)) =
-                        provider.get_transaction_receipt(transaction.hash).await
-                    {
-                        let logs = receipt.logs;
-                        if logs.is_empty() {
-                            return;
+                let mut futures = FuturesUnordered::new();
+                for transaction in block.transactions {
+                    let provider = provider.clone();
+                    let pool_clone = pool.clone();
+
+                    futures.push(task::spawn(async move {
+                        if let Ok(Some(receipt)) =
+                            provider.get_transaction_receipt(transaction.hash).await
+                        {
+                            let logs = receipt.logs;
+                            if logs.is_empty() {
+                                return;
+                            }
+                            let mut conn =
+                                pool_clone.get().await.expect("Pool connection Error");
+
+                            let summary_logs: Vec<SummaryLog> =
+                                logs.iter().map(|log| log.clone().into()).collect();
+                            let message: String =
+                                serde_json::to_string(&summary_logs).unwrap();
+
+                            let _: () = conn
+                                .xadd(
+                                    "ASSETS_INDEXER_STREAM",
+                                    "*",
+                                    &[("message", &message)],
+                                )
+                                .await
+                                .expect("Error sending message");
                         }
-                        let mut conn =
-                            pool_clone.get().await.expect("Pool connection Error");
+                    }));
 
-                        let summary_logs: Vec<SummaryLog> =
-                            logs.iter().map(|log| log.clone().into()).collect();
-                        let message: String =
-                            serde_json::to_string(&summary_logs).unwrap();
-
-                        let _: () = conn
-                            .xadd("ASSETS_INDEXER_STREAM", "*", &[("message", &message)])
-                            .await
-                            .expect("Error sending message");
+                    if futures.len() >= cpu_cores {
+                        futures.next().await;
                     }
-                }));
-
-                if futures.len() >= cpu_cores {
-                    futures.next().await;
                 }
+
+                while futures.next().await.is_some() {}
             }
-
-            while futures.next().await.is_some() {}
         }
+        start_block = end_block + 1;
+        println!(
+            "Updating start and end block, from {} to {}",
+            start_block, end_block
+        );
     }
-
-    Ok(())
 }
