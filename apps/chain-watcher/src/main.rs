c65e26c47be46bb5275081ed5efe62087c0ee1bb
Joaquin Gonzalez <jpgonzalezra@gmail.com>
2024-03-05 14:38:20 -0300

    use futures in the observer to parallelize the transaction

--- apps/chain-watcher/src/main.rs
+++ apps/chain-watcher/src/main.rs
@@ -1,57 +1,74 @@
-use common::redis::redis_client_factory;
+use common::redis::redis_pool_factory;
 use common::types::SummaryLog;
 use dotenv::dotenv;
 use ethers::providers::{Http, Middleware, Provider};
+use futures::stream::{FuturesUnordered, StreamExt};
 use redis::AsyncCommands;
-use std::env;
-use std::sync::Arc;
-use std::time::Duration;
-use tokio::time::sleep;
+use std::{env, sync::Arc};
+use tokio::task;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
 
-    let mut redis_conn = redis_client_factory()
-        .get_async_connection()
+    let pool = redis_pool_factory()
         .await
         .expect("Error on acquiring redis connection");
-
     let rpc_url = env::var("ETHEREUM_RPC_URL").unwrap();
     let http_provider = Provider::<Http>::try_from(rpc_url)?;
-
     let provider = Arc::new(http_provider);
+
     let start_block = 19249009;
     let end_block = provider.get_block_number().await?;
-
     println!("Indexing from block {} to block {}", start_block, end_block);
 
-    for block_number in start_block..=end_block.as_u64() {
-        let block = provider.get_block_with_txs(block_number).await?;
+    let cpu_cores = num_cpus::get();
+    println!("Using {} workers.", cpu_cores);
 
-        if let Some(block) = block {
+    for block_number in start_block..=end_block.as_u64() {
+        let provider = provider.clone();
+
+        if let Ok(Some(block)) = provider.get_block_with_txs(block_number).await {
             println!(
                 "Block Number {}: {} transactions",
                 block.number.unwrap(),
                 block.transactions.len()
             );
 
+            let mut futures = FuturesUnordered::new();
             for transaction in block.transactions {
-                let tx_receipt =
-                    provider.get_transaction_receipt(transaction.hash).await?;
-                if let Some(receipt) = tx_receipt {
-                    for log in receipt.logs {
-                        let summary_log: SummaryLog = log.into();
-                        let message = serde_json::to_string(&summary_log).unwrap();
-                        let _: () = redis_conn
-                            .xadd("ASSETS_INDEXER_STREAM", "*", &[("message", &message)])
-                            .await
-                            .unwrap();
+                let provider = provider.clone();
+                let pool_clone = pool.clone();
+
+                futures.push(task::spawn(async move {
+                    if let Ok(Some(receipt)) =
+                        provider.get_transaction_receipt(transaction.hash).await
+                    {
+                        for log in receipt.logs {
+                            let summary_log: SummaryLog = log.into();
+                            let message: String =
+                                serde_json::to_string(&summary_log).unwrap();
+                            let mut conn =
+                                pool_clone.get().await.expect("Pool connection Error");
+                            let _: () = conn
+                                .xadd(
+                                    "ASSETS_INDEXER_STREAM",
+                                    "*",
+                                    &[("message", &message)],
+                                )
+                                .await
+                                .expect("Error sending message");
+                        }
                     }
+                }));
+
+                if futures.len() >= cpu_cores {
+                    futures.next().await;
                 }
             }
+
+            while futures.next().await.is_some() {}
         }
-        sleep(Duration::from_secs(1)).await;
     }
 
     Ok(())
