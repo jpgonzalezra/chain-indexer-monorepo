8f1ff3b53919d9e8f3875dadc87f3cfe72e00f55
Joaquin Gonzalez <jpgonzalezra@gmail.com>
2024-03-18 22:32:10 -0300

    event processor service refactor

--- apps/assets-indexer/src/services/proccesors/event_processor.rs
+++ apps/assets-indexer/src/services/proccesors/event_processor.rs
@@ -1,13 +1,53 @@
+use std::fmt;
+
 use async_trait::async_trait;
+
+#[derive(Debug)]
+pub enum ProcessorError {
+    ParseError(String, String),
+    DecodeError(String, String),
+    ValidationError(String),
+    DatabaseError(String),
+}
+
+pub enum ProcessResult {
+    Stored,
+    NotApplicable,
+}
+
+impl fmt::Display for ProcessorError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            ProcessorError::ParseError(error, data) => {
+                write!(f, "Parse Error: {}. Data: {}", error, data)
+            }
+            ProcessorError::DecodeError(error, data) => {
+                write!(f, "Decode Error: {}. Data: {}", error, data)
+            }
+            ProcessorError::ValidationError(ref err) => {
+                write!(f, "Validation Error: {}", err)
+            }
+            ProcessorError::DatabaseError(ref err) => {
+                write!(f, "Database Error: {}", err)
+            }
+        }
+    }
+}
 
 #[async_trait]
 pub trait EventProcessor {
-    async fn store_if_apply(&self, event: &EventProcessorRequest) -> bool;
+    async fn store_if_apply(
+        &self,
+        event: &EventProcessorRequest,
+    ) -> Result<ProcessResult, ProcessorError>;
 }
 
+#[derive(Debug)]
 pub struct EventProcessorRequest {
     pub address: String,
     pub data: String,
+    pub chain_id: u32,
+    pub block_number: u64,
     pub topic0: String,
     pub topic1: Option<String>,
     pub topic2: Option<String>,
@@ -31,7 +71,17 @@ impl EventProcessorService {
 
     pub async fn process_and_store_if_apply(&self, event: &EventProcessorRequest) {
         for processor in &self.processors {
-            processor.store_if_apply(event).await;
+            match processor.store_if_apply(event).await {
+                Ok(ProcessResult::Stored) => {
+                    println!("Event stored successfully {:?}", event);
+                }
+                Ok(ProcessResult::NotApplicable) => {
+                    // no-op
+                }
+                Err(e) => {
+                    eprintln!("Error processing event: {:?}", e);
+                }
+            }
         }
     }
 }
