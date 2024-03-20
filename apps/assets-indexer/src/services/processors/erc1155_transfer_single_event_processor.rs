1d0def96e35985e4b0fc744a5e896b683cd48fad
Joaquin Gonzalez <jpgonzalezra@gmail.com>
2024-03-20 13:31:11 -0300

    refactor: Improve address parsing and conversion logic

--- apps/assets-indexer/src/services/processors/erc1155_transfer_single_event_processor.rs
+++ apps/assets-indexer/src/services/processors/erc1155_transfer_single_event_processor.rs
@@ -1,4 +1,5 @@
 use async_trait::async_trait;
+use ethers::types::H256;
 
 use super::event_processor::{
     EventProcessor, EventProcessorRequest, ProcessResult, ProcessorError,
@@ -56,16 +57,89 @@ impl EventProcessor for Erc1155TransferSingleProcessor {
             .await
             .map_err(|e| ProcessorError::DatabaseError(e.to_string()))?;
 
-        let id_uint = id.clone().as_u128();
-        let from;
-        let to;
+        let from_address_bytes = array_bytes::hex_n_into::<String, H256, 32>(
+            event.topic3.clone().ok_or_else(|| {
+                ProcessorError::ValidationError(
+                    "Missing 'from' address topic".to_string(),
+                )
+            })?,
+        )
+        .map_err(|_| {
+            ProcessorError::ParseError(
+                "Failed to parse 'from' address".to_string(),
+                "".to_string(),
+            )
+        })?;
+
+        let from_address_tokens =
+            ethabi::decode(&[ParamType::Address], from_address_bytes.as_bytes())
+                .map_err(|e| {
+                    ProcessorError::DecodeError(
+                        "Failed to decode 'from' address".to_string(),
+                        e.to_string(),
+                    )
+                })?;
+
+        let from_address = from_address_tokens.first().ok_or_else(|| {
+            ProcessorError::ValidationError(
+                "Missing 'from' address in tokens".to_string(),
+            )
+        })?;
+
+        let from = format!(
+            "0x{}",
+            hex::encode(from_address.clone().into_address().ok_or_else(|| {
+                ProcessorError::ParseError(
+                    "Failed to convert from_address into string".to_string(),
+                    "".to_string(),
+                )
+            })?)
+        );
+
+        let to_address_bytes = array_bytes::hex_n_into::<String, H256, 32>(
+            event.topic3.clone().ok_or_else(|| {
+                ProcessorError::ValidationError("Missing 'to' address topic".to_string())
+            })?,
+        )
+        .map_err(|_| {
+            ProcessorError::ParseError(
+                "Failed to parse 'to' address".to_string(),
+                "".to_string(),
+            )
+        })?;
+
+        let to_address_tokens =
+            ethabi::decode(&[ParamType::Address], to_address_bytes.as_bytes()).map_err(
+                |e| {
+                    ProcessorError::DecodeError(
+                        "Failed to decode 'to' address".to_string(),
+                        e.to_string(),
+                    )
+                },
+            )?;
+
+        let to_address = to_address_tokens.first().ok_or_else(|| {
+            ProcessorError::ValidationError("Missing 'to' address in tokens".to_string())
+        })?;
+
+        let to = format!(
+            "0x{}",
+            hex::encode(to_address.clone().into_address().ok_or_else(|| {
+                ProcessorError::ParseError(
+                    "Failed to convert to_address into string".to_string(),
+                    "".to_string(),
+                )
+            })?)
+        );
+        let id = id.clone().to_string();
+
         let transfer_data = Erc1155TransferData {
             contract_id,
             block_number: event.block_number as i32,
             from,
             to,
-            token_ids: [id_uint.to_string()].to_vec(),
-            amounts: [amount.clone().as_u128().to_string()].to_vec(),
+            token_ids: [id].to_vec(),
+            amounts: [amount.clone().to_string()].to_vec(),
         };
 
         self.erc1155_repository
