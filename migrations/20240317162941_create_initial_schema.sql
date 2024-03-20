2cf3fa31bf5ba1e13a90d201ab70babc932cb8e7
Joaquin Gonzalez <jpgonzalezra@gmail.com>
2024-03-20 16:19:16 -0300

    style: Add missing commas to SQL statements

--- migrations/20240317162941_create_initial_schema.sql
+++ migrations/20240317162941_create_initial_schema.sql
@@ -9,7 +9,7 @@ CREATE TABLE contract (
 CREATE TABLE block (
     block_number BIGINT NOT NULL,
     chain_id INTEGER NOT NULL,
-    hash VARCHAR(255) NOT NULL UNIQUE
+    hash VARCHAR(255) NOT NULL UNIQUE,
     PRIMARY KEY (chain_id, block_number)
 );
 
@@ -19,7 +19,7 @@ CREATE TABLE erc721_transfer (
     chain_id INTEGER NOT NULL,
     block_number BIGINT NOT NULL,
     tx_hash VARCHAR(255) NOT NULL, 
-    tx_index BIGINT NOT NULL;
+    tx_index BIGINT NOT NULL,
     "from" VARCHAR(255) NOT NULL,
     "to" VARCHAR(255) NOT NULL,
     token_id VARCHAR NOT NULL,
@@ -33,7 +33,7 @@ CREATE TABLE erc1155_transfer (
     chain_id INTEGER NOT NULL,
     block_number BIGINT NOT NULL,
     tx_hash VARCHAR(255) NOT NULL,
-    tx_index BIGINT NOT NULL;
+    tx_index BIGINT NOT NULL,
     amounts VARCHAR[] NOT NULL,
     token_ids VARCHAR[] NOT NULL,
     "from" VARCHAR(255) NOT NULL,
