234544a0c06d771550c0e6e0ad9ba774c87e1057
Joaquin Gonzalez <jpgonzalezra@gmail.com>
2024-03-26 11:50:13 -0300

    style: Update log file names in main.rs files

--- apps/chain-watcher/src/main.rs
+++ apps/chain-watcher/src/main.rs
@@ -17,7 +17,7 @@ use tracing_appender::rolling;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let config: Config = Config::new();
 
-    let file_appender = rolling::daily("./logs", "prefix.log");
+    let file_appender = rolling::never("./logs", "chain-watcher.log");
     let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
 
     let debug_level = if config.debug { "debug" } else { "info" };
